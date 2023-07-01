4eec7d8e0150747c2ed884772a70a07a369dbd5b
arjuncvinod <arjuncvinod007@gmail.com>
2023-07-02 00:21:36 +0530

    Entry componet created

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Entry from "./Entry";
 
 function App() {
   return (
@@ -8,44 +9,8 @@ function App() {
       </h1>
 
       <dl className="dictionary">
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              💪
-            </span>
-            <span>Tense Biceps</span>
-          </dt>
-          <dd>
-            “You can do that!” or “I feel strong!” Arm with tense biceps. Also
-            used in connection with doing sports, e.g. at the gym.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🙏
-            </span>
-            <span>Person With Folded Hands</span>
-          </dt>
-          <dd>
-            Two hands pressed together. Is currently very introverted, saying a
-            prayer, or hoping for enlightenment. Is also used as a “high five”
-            or to say thank you.
-          </dd>
-        </div>
-        <div className="term">
-          <dt>
-            <span className="emoji" role="img" aria-label="Tense Biceps">
-              🤣
-            </span>
-            <span>Rolling On The Floor, Laughing</span>
-          </dt>
-          <dd>
-            This is funny! A smiley face, rolling on the floor, laughing. The
-            face is laughing boundlessly. The emoji version of “rofl“. Stands
-            for „rolling on the floor, laughing“.
-          </dd>
-        </div>
+        
+       <Entry />
       </dl>
     </div>
   );
---
+++ src/components/Entry.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function entry(props){
+    return (
+      <div className="term">
+        <dt>
+          <span className="emoji" role="img" aria-label="Tense Biceps">
+            💪
+          </span>
+          <span>Tense Biceps</span>
+        </dt>
+        <dd>
+          “You can do that!” or “I feel strong!” Arm with tense biceps. Also
+          used in connection with doing sports, e.g. at the gym.
+        </dd>
+      </div>
+    );
+}
+export default entry;
